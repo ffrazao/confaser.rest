bd85455861c1d1e9a9119cfea6849f2f5528ecdb
Fernando Frazão da Silva <fernando.frazao@emater.df.gov.br>
2021-02-14 10:11:57 -0300

    Versao inicial com o spring boot atual
    Signed-off-by: Fernando Frazão da Silva <fernando.frazao@emater.df.gov.br>

---
+++ src/main/java/br/com/asser/confaser/ConfaserApplication.java
@@ -0,0 +1,13 @@
+package br.com.asser.confaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfaserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfaserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/asser/confaser/ConfaserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.asser.confaser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfaserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
